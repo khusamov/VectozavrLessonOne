52089297a08a1f97a458febda09ee5e9d338b55f
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-10 13:48:41 +0300

    Добавлена операция нормализации векторов

---
+++ VectozavrLessonOne/Algebra/Vector/VectorNormalization.cs
@@ -0,0 +1,22 @@
+﻿namespace VectozavrLessonOne.Algebra.Vector
+{
+	public partial class Vector
+	{
+		/// <summary>
+		/// Нормализация вектора. 
+		/// Уменьшение длины вектора до единичной при сохранении направления.
+		/// Для нормализации вектора достаточно вычислить его длину и разделить каждый компонент на эту длину.
+		/// </summary>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException"></exception>
+		public Vector Normalize()
+		{
+			if (Length == 0)
+			{
+				throw new ArgumentException("Нельзя нормализовать нулевой вектор");
+			}
+
+			return this / Length;
+		}
+	}
+}
---
+++ VectozavrLessonOneTests/Algebra/Vector/VectorNormalizationTest.cs
@@ -0,0 +1,25 @@
+﻿namespace VectozavrLessonOneTests.Algebra.Vector
+{
+	public partial class VectorTest
+	{
+		[TestMethod]
+		public void NormalizationTest()
+		{
+			AlgebraVector.Vector vector = new(new float[] { 5, -2, 7 });
+			AlgebraVector.Vector expected = new(
+				new float[] 
+				{
+					5f / (float)Math.Sqrt(78f), 
+					-(float)Math.Sqrt(2f / 39f), 
+					7f / (float)Math.Sqrt(78f)
+				}
+			);
+			AlgebraVector.Vector result = vector.Normalize();
+
+			for (int row = 0; row < result.Rows; row++)
+			{
+				Assert.AreEqual(expected[row], result[row], floatDelta, $"Ошибка на строке {row}.");
+			}
+		}
+	}
+}
