e7a83d7723e1ec604ce87973be033f4a4e21b71f
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-10 19:57:35 +0300

    Добавлены матрицы проекции и экрана

---
+++ VectozavrLessonOne/Algebra/Matrix/MatrixProjection.cs
@@ -0,0 +1,26 @@
+﻿namespace VectozavrLessonOne.Algebra.Matrix
+{
+	public partial class Matrix
+	{
+		/// <summary>
+		/// Матрица проекции точки на виртуальный нормированный экран (размеры от -1 до 1).
+		/// </summary>
+		/// <param name="fov">Поле зрения (угол обзора) в радианах</param>
+		/// <param name="aspect">Соотношение сторон экрана w/h</param>
+		/// <param name="znear"></param>
+		/// <param name="zfar"></param>
+		/// <returns></returns>
+		public static Matrix Projection(float fov, float aspect, float znear, float zfar)
+		{
+			return new(
+				new float[,]
+				{
+					{ 1 / (MathF.Tan(fov / 2) * aspect), 0, 0, 0 },
+					{ 0, 1 / MathF.Tan(fov / 2), 0, 0 },
+					{ 0, 0, zfar / (zfar - znear), -zfar * znear / (zfar - znear) },
+					{ 0, 0, 1, 0 }
+				}
+			);
+		}
+	}
+}
---
+++ VectozavrLessonOne/Algebra/Matrix/MatrixScreen.cs
@@ -0,0 +1,24 @@
+﻿namespace VectozavrLessonOne.Algebra.Matrix
+{
+	public partial class Matrix
+	{
+		/// <summary>
+		/// Матрица для проекции точки с нормированного экрана на реальный экран монитора.
+		/// </summary>
+		/// <param name="width">Ширина экрана в пикселах</param>
+		/// <param name="height">Высота экрана в пикселах</param>
+		/// <returns></returns>
+		public static Matrix Screen(int width, int height)
+		{
+			return new(
+				new float[,]
+				{
+					{ -0.5f * width, 0, 0, 0.5f * width },
+					{ 0, -0.5f * height, 0, 0.5f * height },
+					{ 0, 0, 1, 0 },
+					{ 0, 0, 0, 1 }
+				}
+			);
+		}
+	}
+}
