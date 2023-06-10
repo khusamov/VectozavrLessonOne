4e9f15dcb35462d22cd74669ec3e943f0c412b97
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-10 13:59:25 +0300

    Добавлена операция создания матрицы вращения вокруг определенного вектора

---
+++ VectozavrLessonOne/Algebra/Vector/VectorRotation.cs
@@ -0,0 +1,16 @@
+﻿using AlgebraMatrix = VectozavrLessonOne.Algebra.Matrix;
+
+namespace VectozavrLessonOne.Algebra.Vector
+{
+	public partial class Vector
+	{
+		/// <summary>
+		/// Создать матрицу вращения вокруг данного вектора.
+		/// </summary>
+		/// <returns>Матрица</returns>
+		public AlgebraMatrix.Matrix Rotation()
+		{
+			return RotationX(X) * RotationY(Y) * RotationZ(Z);
+		}
+	}
+}
