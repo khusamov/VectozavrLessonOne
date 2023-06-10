62f6bb3b3f60290c3b20f78c53928f64f5f35490
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-10 20:29:45 +0300

    Восстановлен класс Timer

---
+++ VectozavrLessonOne/Engine/Utils/Timer.cs
@@ -0,0 +1,56 @@
+﻿namespace VectozavrLessonOne.Engine.Utils
+{
+	/// <summary>
+	/// Таймер.
+	/// </summary>
+	/// <see cref="https://github.com/vectozavr/learn_3dzavr/blob/master/engine/utils/Timer.h"/>
+	/// <see cref="https://github.com/vectozavr/learn_3dzavr/blob/master/engine/utils/Timer.cpp"/>
+	internal class Timer
+	{
+		private DateTime startTime = new();
+		private DateTime endTime = new();
+		private bool isRunning = false;
+
+		/// <summary>
+		/// Запустить таймер.
+		/// </summary>
+		public void Start()
+		{
+			startTime = DateTime.Now;
+			isRunning = true;
+		}
+
+		/// <summary>
+		/// Остановить таймер.
+		/// </summary>
+		public void Stop()
+		{
+			endTime = DateTime.Now;
+			isRunning = false;
+		}
+
+		/// <summary>
+		/// Получить число секунд с момента запуска таймера.
+		/// Если таймер работает, то с момента запуска таймера до текущего момента.
+		/// Если таймер выключен, то с момента запуска таймера до момента, когда его выключили.
+		/// </summary>
+		public int ElapsedSeconds
+		{
+			get => ElapsedMilliseconds / 1000;
+		}
+
+		/// <summary>
+		/// Получить число миллисекунд с момента запуска таймера.
+		/// Если таймер работает, то с момента запуска таймера до текущего момента.
+		/// Если таймер выключен, то с момента запуска таймера до момента, когда его выключили.
+		/// </summary>
+		public int ElapsedMilliseconds
+		{
+			get
+			{
+				DateTime _endTime = isRunning ? DateTime.Now : endTime;
+				return _endTime.Millisecond - startTime.Millisecond;
+			}
+		}
+	}
+}
