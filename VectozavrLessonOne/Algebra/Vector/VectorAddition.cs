08106d1b9218a687480b009116fa645477d7b951
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-10 11:40:35 +0300

    Добавлена операция сложения векторов

---
+++ VectozavrLessonOne/Algebra/Vector/VectorAddition.cs
@@ -0,0 +1,21 @@
+﻿namespace VectozavrLessonOne.Algebra.Vector
+{
+	public partial class Vector
+	{
+		/// <summary>
+		/// Сложение векторов.
+		/// </summary>
+		/// <param name="left"></param>
+		/// <param name="right"></param>
+		/// <returns></returns>
+		public static Vector operator +(Vector left, Vector right)
+		{
+			if (left.Dimensions != right.Dimensions)
+			{
+				throw new ArgumentException("Векторы должны иметь один размер");
+			}
+
+			return FromMatrix(left + right);
+		}
+	}
+}
