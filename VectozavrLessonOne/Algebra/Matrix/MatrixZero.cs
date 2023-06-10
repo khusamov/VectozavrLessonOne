45092176d76dc79d4962c23af820a7cbad5ca972
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-10 14:06:33 +0300

    Упрощен код

--- VectozavrLessonOne/Algebra/Matrix/MatrixZero.cs
+++ VectozavrLessonOne/Algebra/Matrix/MatrixZero.cs
@@ -5,9 +5,18 @@
 		/// <summary>
 		/// Нулевая матрица.
 		/// </summary>
-		/// <param name="rows">Размер матрицы</param>
-		/// <param name="cols">Размер матрицы</param>
+		/// <param name="rows">Количество строк матрицы</param>
+		/// <param name="cols">Количество столбцов матрицы. Можно не указывать, тогда будет квадратная матрица</param>
 		/// <returns>Матрица</returns>
-		public static Matrix Zero(int rows, int? cols = null) => new(new float[rows, cols is null ? rows : (int)cols]);
+		public static Matrix Zero(int rows, int? cols = null)
+		{
+			int _cols = (
+				cols is null 
+					? rows 
+					: (int)cols
+			);
+
+			return new(new float[rows, _cols]);
+		}
 	}
 }
