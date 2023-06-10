3547efe615a26c06ae10fd20d4edef45feddbe66
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-10 13:10:50 +0300

    Добавлено скалярное произведение векторов

---
+++ VectozavrLessonOne/Algebra/Vector/VectorDot.cs
@@ -0,0 +1,28 @@
+﻿namespace VectozavrLessonOne.Algebra.Vector
+{
+	public partial class Vector
+	{
+		/// <summary>
+		/// Скалярное произведение векторов.
+		/// </summary>
+		/// <param name="vector"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException"></exception>
+		public float Dot(Vector vector)
+		{
+			if (Dimensions != vector.Dimensions)
+			{
+				throw new ArgumentException("Размерность векторов должна быть одинаковой");
+			}
+
+			float result = 0.0f;
+
+			for (int i = 0; i < Dimensions; i++)
+			{
+				result += this[i] * vector[i];
+			}
+
+			return result;
+		}
+	}
+}
---
+++ VectozavrLessonOneTests/Algebra/Vector/VectorDotTest.cs
@@ -0,0 +1,16 @@
+﻿namespace VectozavrLessonOneTests.Algebra.Vector
+{
+	public partial class VectorTest
+	{
+		[TestMethod]
+		public void DotTest()
+		{
+			AlgebraVector.Vector left = new(new float[] { 1, 2, 3 });
+			AlgebraVector.Vector right = new(new float[] { 4, 5, 6 });
+			float expected = 32.0f;
+			float result = left.Dot(right);
+
+			Assert.AreEqual(expected, result, floatDelta);
+		}
+	}
+}
