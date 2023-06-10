c35a541ff7a534835d60ab81c2eb2bcfab8d0c1c
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-10 12:32:42 +0300

    Добавлены операции создания нулевой матрицы и вектора

---
+++ VectozavrLessonOne/Algebra/Matrix/MatrixZero.cs
@@ -0,0 +1,13 @@
+﻿namespace VectozavrLessonOne.Algebra.Matrix
+{
+	public partial class Matrix
+	{
+		/// <summary>
+		/// Нулевая матрица.
+		/// </summary>
+		/// <param name="rows">Размер матрицы</param>
+		/// <param name="cols">Размер матрицы</param>
+		/// <returns>Матрица</returns>
+		public static Matrix Zero(int rows, int? cols) => new(new float[rows, cols is null ? rows : (int)cols]);
+	}
+}
---
+++ VectozavrLessonOne/Algebra/Vector/VectorZero.cs
@@ -0,0 +1,12 @@
+﻿namespace VectozavrLessonOne.Algebra.Vector
+{
+	public partial class Vector
+	{
+		/// <summary>
+		/// Нулевой вектор.
+		/// </summary>
+		/// <param name="size">Размер вектора</param>
+		/// <returns>Матрица</returns>
+		public static Vector Zero(int rows) => new(new float[rows]);
+	}
+}
