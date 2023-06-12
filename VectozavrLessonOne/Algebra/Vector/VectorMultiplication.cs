12a56b8b3765e89e7c521263f2e602b395e4a31b
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-12 18:20:46 +0300

    Добавлена операция умножения матрицы на вектор-столбец

--- VectozavrLessonOne/Algebra/Vector/VectorMultiplication.cs
+++ VectozavrLessonOne/Algebra/Vector/VectorMultiplication.cs
@@ -1,4 +1,6 @@
-﻿namespace VectozavrLessonOne.Algebra.Vector
+﻿using AlgebraMatrix = VectozavrLessonOne.Algebra.Matrix;
+
+namespace VectozavrLessonOne.Algebra.Vector
 {
 	public partial class Vector
 	{
@@ -9,5 +11,19 @@
 		/// <param name="right"></param>
 		/// <returns></returns>
 		public static Vector operator *(Vector left, Vector right) => left.Cross(right);
+
+		/// <summary>
+		/// Умножение матрицы на вектор-столбец.
+		/// На выходе возвращается именно вектор-столбец, 
+		/// потому что эта операция предназначена для трансформации 
+		/// вектора при помощи матрицы-трансформации.
+		/// </summary>
+		/// <param name="left">Матрица</param>
+		/// <param name="right">Вектор</param>
+		/// <returns>Вектор</returns>
+		public static Vector operator *(AlgebraMatrix.Matrix left, Vector right)
+		{
+			return FromMatrix(left * right.ToMatrix());
+		}
 	}
 }
