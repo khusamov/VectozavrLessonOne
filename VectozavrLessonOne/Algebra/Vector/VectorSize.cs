86e3bfa20849c3cf6da652ef638d0ee9d693d89f
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-12 13:43:16 +0300

    Добавлен класс для определения размера вектора

---
+++ VectozavrLessonOne/Algebra/Vector/VectorSize.cs
@@ -0,0 +1,28 @@
+﻿namespace VectozavrLessonOne.Algebra.Vector
+{
+	/// <summary>
+	/// Размер вектора-столбца.
+	/// </summary>
+	public class VectorSize
+	{
+		private readonly int rows = 0;
+
+		/// <summary>
+		/// Количество строк вектора.
+		/// </summary>
+		/// <returns></returns>
+		public int Rows
+		{
+			get => rows;
+		}
+
+		/// <summary>
+		/// Конструктор размеров вектора-столбца.
+		/// </summary>
+		/// <param name="rows">Количество строк вектора.</param>
+		public VectorSize(int rows)
+		{
+			this.rows = rows;
+		}
+	}
+}
