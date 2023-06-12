c18b4be5f5ad7e62e2d8034bb7a4a9c94233faef
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-12 18:21:22 +0300

    Внесены массовые исправления в треугольники

--- VectozavrLessonOne/Engine/Triangle.cs
+++ VectozavrLessonOne/Engine/Triangle.cs
@@ -5,7 +5,9 @@ using VectozavrLessonOne.Algebra.Vector;
 namespace VectozavrLessonOne.Engine
 {
 	/// <summary>
-	/// Triangle
+	/// Треугольник. 
+	/// Это набор из трех векторов (вершины треугольника), 
+	/// которые образуют поверхность и цвет этой поверхности.
 	/// </summary>
 	/// <see cref="https://github.com/vectozavr/learn_3dzavr/blob/master/engine/Triangle.h"/>
 	/// <see cref="https://github.com/vectozavr/learn_3dzavr/blob/master/engine/Triangle.cpp"/>
@@ -16,12 +18,12 @@ namespace VectozavrLessonOne.Engine
 		/// <summary>
 		/// Список вершин треугольника. Вершины хранятся как трехмерные векторы. 
 		/// </summary>
-		private readonly List<Vector> _points = new();
+		private readonly Vector[] _points = Array.Empty<Vector>();
 		
 		/// <summary>
 		/// Нормаль треугольника. Трехмерный вектор.
 		/// </summary>
-		private Vector _normal = Vector.Zero(new VectorSize(3));
+		private readonly Vector _normal = Vector.Zero(new VectorSize(3));
 
 		/// <summary>
 		/// Вычисление нормали треугольника.
@@ -29,6 +31,8 @@ namespace VectozavrLessonOne.Engine
 		/// <returns></returns>
 		private Vector CalculateNormal()
 		{
+			// Считаем, что порядок расположения вершин треугольника в массиве идет
+			// по часовой стрелке относительно нормали (в соответствии с правилом буравчика или правилом правой руки).
 			Vector vector1 = this[1] - this[0];
 			Vector vector2 = this[2] - this[0];
 			Vector crossProduct = vector1 * vector2;
@@ -43,16 +47,14 @@ namespace VectozavrLessonOne.Engine
 		/// <param name="point3">Третья вершина треугольника</param>
 		/// <param name="color">Цвет поверхности треугольника</param>
 		/// <exception cref="ArgumentException"></exception>
-		public Triangle(Vector point1, Vector point2, Vector point3, Color color)
+		public Triangle(Vector point1, Vector point2, Vector point3, Color? color = null)
 		{
 			if (point1.Dimensions != 3 || point2.Dimensions != 3 || point3.Dimensions != 3)
 			{
 				throw new ArgumentException("Вершины треугольника должны быть трехмерными");
 			}
-			_points.Add(point1);
-			_points.Add(point2);
-			_points.Add(point3);
-			_color = color;
+			_points = new Vector[] { point1, point2, point3 };
+			_color = color is null ? Color.White : (Color)color;
 			_normal = CalculateNormal();
 		}
 
@@ -69,7 +71,14 @@ namespace VectozavrLessonOne.Engine
 		/// <returns></returns>
 		public Vector this[int pointIndex]
 		{
-			get => _points[pointIndex];
+			get
+			{
+				if (pointIndex < 0 || pointIndex > 2)
+				{
+					throw new IndexOutOfRangeException("Индекс можно задавать в диапазоне от 0 до 2");
+				}
+				return _points[pointIndex];
+			}
 		}
 
 		/// <summary>
@@ -104,7 +113,7 @@ namespace VectozavrLessonOne.Engine
 				throw new ArgumentException("Матрица должна быть 4 на 4");
 			}
 
-			Vector ToVector4(Vector vector, float w = 0)
+			Vector ToVector4(Vector vector, float w = 1)
 			{
 				if (vector.Dimensions != 3)
 				{
@@ -113,10 +122,19 @@ namespace VectozavrLessonOne.Engine
 				return new Vector(new float[] { vector.X, vector.Y, vector.Z, w });
 			}
 
+			Vector ToVector3(Vector vector)
+			{
+				if (vector.Dimensions != 4)
+				{
+					throw new ArgumentException("Вектор должен быть четырехмерным");
+				}
+				return new Vector(new float[] { vector.X, vector.Y, vector.Z });
+			}
+
 			return new(
-				Vector.FromMatrix(matrix * ToVector4(triangle[0]).ToMatrix()),
-				Vector.FromMatrix(matrix * ToVector4(triangle[1]).ToMatrix()),
-				Vector.FromMatrix(matrix * ToVector4(triangle[2]).ToMatrix()),
+				ToVector3(matrix * ToVector4(triangle[0])),
+				ToVector3(matrix * ToVector4(triangle[1])),
+				ToVector3(matrix * ToVector4(triangle[2])),
 				triangle.Color
 			);
 		}
@@ -127,9 +145,8 @@ namespace VectozavrLessonOne.Engine
 		/// Внимание, перед проверкой необходимо найти точку на плоскости треугольника. 
 		/// Например точку пересечения прямой и плоскости треугольника.
 		/// 
-		/// Похоже эта функция проверяет наличие точки внутри призмы, 
+		/// Эта функция проверяет наличие точки внутри призмы, 
 		/// которая получается вытягиванием треугольника вдоль нормали треугольника.
-		/// Но это надо еще проверить.
 		/// </summary>
 		/// <param name="point">Тестируемая точка</param>
 		/// <returns></returns>
@@ -149,9 +166,7 @@ namespace VectozavrLessonOne.Engine
 		}
 
 		/// <summary>
-		/// Явно это вычисление позиции треугольника. Но не ясно почему она именно так вычисляется.
-		/// Вопрос задан на форуме. Возможно автор прояснит ситуацию.
-		/// Данный метод используется, поэтому надо выяснить этот вопрос.
+		/// Координаты центра треугольника.
 		/// </summary>
 		/// <see cref="https://vectozavr.ru/forum?post=57"/>
 		/// <returns></returns>
@@ -161,20 +176,19 @@ namespace VectozavrLessonOne.Engine
 		}
 
 		/// <summary>
-		/// Пока не ясно что делает этот метод.
-		/// Похоже этот метод нигде не используется.
+		/// Расстояние от плоскости треугольника до точки.
 		/// </summary>
-		/// <param name="vector"></param>
+		/// <param name="point"></param>
 		/// <returns></returns>
 		/// <exception cref="ArgumentException"></exception>
-		public float Distance(Vector vector)
+		public float Distance(Vector point)
 		{
-			if (vector.Dimensions != 3)
+			if (point.Dimensions != 3)
 			{
-				throw new ArgumentException("Вектор должен быть трехмерным");
+				throw new ArgumentException("Точка должна быть трехмерной");
 			}
 
-			return Normal.Dot(this[0] - vector);
+			return Normal.Dot(this[0] - point);
 		}
 	}
 }
