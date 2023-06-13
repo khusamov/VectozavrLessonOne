b13278694a1395fc1760551f11142182638c25aa
Khusamov Sukhrob <khusamov@yandex.ru>
2023-06-13 23:38:38 +0300

    Добавлена заготовка класса Plane

---
+++ VectozavrLessonOne/Algebra/Plane/Plane.cs
@@ -0,0 +1,10 @@
+﻿namespace VectozavrLessonOne.Algebra.Plane
+{
+	/// <summary>
+	/// Плоскость. 
+	/// Описывается вектором нормали и одной точкой на этой плоскости.
+	/// </summary>
+	public partial class Plane
+	{
+	}
+}
